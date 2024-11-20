3780990425be06c7c782f85777d905f7b2b93e59
Im-younique <dyyim4725@gmail.com>
2024-11-20 19:20:13 +0900

    [add] vuetify 세팅

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import router from './router'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).use(router).mount('#app')
+import router from "./router";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(router).use(vuetify).mount("#app");
