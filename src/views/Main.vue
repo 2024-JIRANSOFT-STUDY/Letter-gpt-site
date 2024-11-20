bc3efb87641ee820b54d4e2a8b9e738712f5d2e5
Im-younique <dyyim4725@gmail.com>
2024-11-20 18:24:25 +0900

    [chore] sidebar 및 layout 병합

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,17 +1,26 @@
 <template>
-    <div class="main">
-      <Sidebar />
-    </div>
+  <div class="main">
+    <Sidebar />
+    <MainLayout>
+      <template #children>
+        <PromptForm></PromptForm>
+        <PromptResult></PromptResult>
+      </template>
+    </MainLayout>
+  </div>
 </template>
-  
+
 <script setup>
-  import Sidebar from '../components/Sidebar.vue';
+import Sidebar from "../components/Sidebar.vue";
+import MainLayout from "../components/Layout/MainLayout.vue";
+import PromptForm from "../components/PromptForm.vue";
+import PromptResult from "../components/PromptResult.vue";
 </script>
-  
+
 <style scoped lang="scss">
-  .main {
-    display: flex;
-    width: 100%;
-    height: 100%;
-  }
+.main {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
 </style>
