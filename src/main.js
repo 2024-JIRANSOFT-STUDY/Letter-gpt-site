b636f7ce362bedab637996e664710d814eb839d1
Im-younique <dyyim4725@gmail.com>
2024-12-09 23:33:42 +0900

    [chore] vuetify theme 비활성화

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
@@ -13,8 +13,9 @@ const vuetify = createVuetify({
   components,
   directives,
   icons: {
-    defaultSet: 'mdi',
-  }
+    defaultSet: "mdi",
+  },
+  theme: false,
 });
 
 createApp(App).use(router).use(vuetify).mount("#app");
